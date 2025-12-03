ed6579514aaa8fe4d838f10213f7e7fd6f1635fb
David Deng <daviddeng@my.smccd.edu>
2025-12-03 12:36:12 -0800

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by David Deng on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
